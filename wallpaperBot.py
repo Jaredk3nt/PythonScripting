4ea8d4054894d1fad7f88de9d330d1b080c81ddb
Jared <jkjones.dev@gmail.com>
2016-01-04 22:46:12 -0600

    added checking on the files in the current dates folder. This will allow you to run the program multiple times per day without retrieving duplicates. Slows it down alittle but its fun to learn what I can do with this. May not be nessicary if I have this bot running daily on a Pi so I may remove it later.

--- wallpaperBot.py
+++ wallpaperBot.py
@@ -15,6 +15,7 @@ user_agent = ("WallpaperGrabber v 0.1")
 r = praw.Reddit(user_agent = user_agent)
 r.login(REDDIT_USERNAME, REDDIT_PASS)
 
+#choose subreddit to pull top photos from
 subreddit = r.get_subreddit("wallpapers")
 
 #creating directory for the date and creating path
@@ -22,30 +23,45 @@ path = ['/home/jared/Pictures/Wallpapers/']
 date = datetime.now().strftime("%y-%d-%m/")
 path.append(date)
 newPath = ''.join(path)
+#if the dir for the current date does not exist creat it
 if not os.path.exists(newPath):
 	os.makedirs(newPath)
-print "Directory created at : ", newPath
+	print "Directory created at : ", newPath
+#create a list of files to make sure you are not duplicating any images
+files = os.listdir(newPath)
 
 #loop over the top posts in wallpapers
-for submission in subreddit.get_hot(limit = 20)\
-	or 'imgur' in submission.domain.split('.'):
-	if submission.url.split('.')[-1] in imageTypes:
-		rq = Request(submission.url)
-    		response = urlopen(rq)
-    		data = response.read()        
-    		try:
-        		im = Image.open(StringIO(data))
-        		im.verify()
-        		#reset list to path
-        		fpath = [newPath]
-        		#get name and type from url
-        		fname = submission.url.split('/')[-1]
-        		fpath.append(fname)
-        		#save file under created path
-        		Image.open(StringIO(data)).save(''.join(fpath))
-        		print "Saved: ", ''.join(fpath)
-    		except:
-        		continue
+for submission in subreddit.get_hot(limit = 20):
+	#if they post contains a photo
+	if submission.url.split('.')[-1] in imageTypes\
+		or 'imgur' in submission.domain.split('.'):
+			rq = Request(submission.url)
+    			response = urlopen(rq)
+    			data = response.read()        
+    			try:
+        			im = Image.open(StringIO(data))
+        			im.verify()
+        			#reset list to path
+        			fpath = [newPath]
+        			#get name and type from url
+        			fname = submission.url.split('/')[-1]
+        			fpath.append(fname)
+
+        			finalPath = ''.join(fpath)
+        			#check for duplicates
+        			new = True
+        			if files:
+        				for name in files:
+        					if (fname == name):
+        						new = False
+        						break
+        			
+        			if new:
+        				Image.open(StringIO(data)).save(finalPath)
+        				print "Saved: ", finalPath
+
+    			except:
+        			continue
 	else:
 		continue
 

c1b093838b19626de9ccdef0e2317d18458d9714
Jared <jkjones.dev@gmail.com>
2016-02-19 12:05:58 -0600

    adding data for current match

--- RiotAPI/champion_data.py
+++ RiotAPI/champion_data.py
@@ -1,4 +1,5 @@
 from cassiopeia import riotapi
+from cassiopeia.type.core.common import Side
 
 def riotapi_setup():
 	riotapi.set_region("NA")
@@ -20,6 +21,32 @@ def ranked_win_percent( championStats ):
 	for championTuple in winPercentList:
 		print "%s : %f" % (championTuple[0], (((championTuple[1].wins * 1.0) / championTuple[1].games_played) * 100))
 
+def get_current_game( summoner ):
+	currentGame = riotapi.get_current_game( summoner )
+	if currentGame:
+		blueSide = []
+		redSide = []
+		#print currentGame.to_json()
+		for player in currentGame.participants:
+			if player.side == Side.blue:
+				blueSide.append(player)
+			else:
+				redSide.append(player)
+		print_players( blueSide, redSide )
+	else:
+		print "That summoner is not currently in a game."
+
+def print_players( blueSide, redSide ):
+	print "Blue Side"
+	print "---------"
+	for player in blueSide:
+		print "%s - %s" % (player.summoner_name, player.champion)
+	print "\nRed Side"
+	print "--------"
+	for player in redSide:
+		print "%s - %s" % (player.summoner_name, player.champion)
+
+#compare to method for the tuple list or champion data (for win percentage)
 def compare_champion_win_percent( champ1, champ2 ):
 	winP1 = (((champ1[1].wins * 1.0) / champ1[1].games_played) * 100)
 	winP2 = (((champ2[1].wins * 1.0) / champ2[1].games_played) * 100)
@@ -29,10 +56,11 @@ def compare_champion_win_percent( champ1, champ2 ):
 	elif winP1 < winP2:
 		return 1
 	else:
-		return 0
+		return 0		
 
 #setup riotapi
 riotapi_setup()
 summoner = get_summoner()
-championStats = get_champion_stats( summoner )
-ranked_win_percent( championStats )
+get_current_game( summoner )
+# championStats = get_champion_stats( summoner )
+# ranked_win_percent( championStats )
